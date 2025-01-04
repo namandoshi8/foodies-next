8366562eeff81dbd540bf78680419a23db10a075
namandoshi8 <workspace.naman@gmail.com>
2025-01-05 02:30:37 +0530

    added share meal form

---
+++ components/meals/image-picker.js
@@ -0,0 +1,62 @@
+"use client";
+
+import { useRef, useState } from "react";
+
+import classes from "./image-picker.module.css";
+import Image from "next/image";
+
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
+  const imageInput = useRef();
+
+  function handlePickClick() {
+    imageInput.current.click();
+  }
+  function handleImageChange(event) {
+    const pickedFile = event.target.files[0];
+
+    if (!pickedFile) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.readAsDataURL(pickedFile);
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {pickedImage ? (
+            <Image src={pickedImage} alt="Picked image" fill />
+          ) : (
+            "No image picked"
+          )}
+        </div>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInput}
+          onChange={handleImageChange}
+          required
+        />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
